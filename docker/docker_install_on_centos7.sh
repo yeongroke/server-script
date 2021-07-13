723286f12612fd45652cebd5d66be5a3e9bfefb4
yeongroke <chfaos153@naver.com>
2021-07-13 22:40:20 +0900

    Update docker_install_on_centos7.sh

--- docker/docker_install_on_centos7.sh
+++ docker/docker_install_on_centos7.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# centos 7 version
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf list docker-ce
+sudo dnf install docker-ce --nobest -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
-echo "yum -y update"
+sudo groupadd docker
+sudo usermod -aG docker selabdev
+newgrp docker
 
-echo "yum -y install docker docekr-registry"
 
-echo "systemctl enable docker.service"
-
-echo "systemctl start docker.service"
-
-echo "docker pull centos"
-
-echo "docker run -i -t centos /bin/bash
+# docker-compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
