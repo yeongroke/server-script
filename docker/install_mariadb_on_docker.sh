2b80a3e61b42136a47028470528d6c31be42f117
yeongroke <chfaos153@naver.com>
2021-07-13 22:43:14 +0900

    Update install_mariadb_on_docker.sh

--- docker/install_mariadb_on_docker.sh
+++ docker/install_mariadb_on_docker.sh
@@ -1,10 +1,13 @@
-docker run -d 
-  --restart="always" 
-  --name docker_mariadb 
-  -p 3307:3306 
-  -v 
-    ${dbdata}:/var/lib/mysql 
-  -v 
-    ${mylocation}:/etc/mysql/my.cnf 
-  -e MYSQL_ROOT_PASSWORD=${password}
-  mariadb:10.4
+#!/bin/bash
+
+docker pull mariadb:10.4
+
+docker run -d --restart unless-stopped \
+  -e TZ=Asia/Seoul \
+  --name docker_mariadb \
+  -p 3306:3306 \
+  -v /docker_data/docker_mariadb:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD=password mariadb:10.4
+
+sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp
+sudo firewall-cmd --reload
