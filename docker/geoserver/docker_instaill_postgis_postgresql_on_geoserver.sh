e5d51962299681d488761686a8e10a4ac890380a
YEONG ROK KIM <chfaos153@naver.com>
2021-12-08 09:27:33 +0900

    Update docker_instaill_postgis_postgresql_on_geoserver.sh

--- docker/geoserver/docker_instaill_postgis_postgresql_on_geoserver.sh
+++ docker/geoserver/docker_instaill_postgis_postgresql_on_geoserver.sh
@@ -1,8 +1,8 @@
 sudo docker run -d --restart unless-stopped --name postgis_db \
         -e TZ=Asia/Seoul \
         -e DEFAULT_ENCODING='UTF8' \
-        -e POSTGRES_USER=selabdev \
-        -e POSTGRES_PASSWORD=qhdkscjfwj\!@ \
+        -e POSTGRES_USER=user \
+        -e POSTGRES_PASSWORD=pwd \
         -v /docker_data/geoserver/postgis_data:/var/lib/postgresql \
         -v /docker_data/geoserver/postgis_config:/etc/postgresql/config \
         -p 5434:5432 \
